05fbb63c48341f4476e8f5bd4743902e84ecbc6f
franticus <70944846+franticus@users.noreply.github.com>
2021-09-01 12:29:15 +0300

    corsefixx

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 8080
 app.use(cors({
     origin: 'https://stark-shelf-84244.herokuapp.com'
 }));
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 app.listen(PORT, () => {
     console.log("Server running");
@@ -22,6 +22,7 @@ app.listen(PORT, () => {
 const filePath = "notes.json";
 app.all('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", 'true');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
