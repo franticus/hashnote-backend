4272067019c9795cdf6936759da24b9b64eb348b
franticus <70944846+franticus@users.noreply.github.com>
2021-09-01 12:10:05 +0300

    firsthh

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 8080
 app.use(cors({
     origin: 'https://stark-shelf-84244.herokuapp.com'
 }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.listen(PORT, () => {
     console.log("Server running");
